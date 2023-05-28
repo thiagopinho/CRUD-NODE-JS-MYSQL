82cb138b25e10f70c01570ddf126dfd7727b847e
Thiago Pinho <thiagopinho05@gmail.com>
2023-05-28 10:10:07 -0300

    feat: creating route /cars to get all cars of database

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const CarController = require('./controllers/CarController');
+
+router.get('/cars', CarController.getAll);
+
 module.exports = router;  
