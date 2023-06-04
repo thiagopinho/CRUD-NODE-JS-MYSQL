093b56cdd9f648ac670326db864c6416010d6bcb
Thiago Pinho <thiagopinho05@gmail.com>
2023-06-04 11:38:50 -0300

    FEAT: creating function to return only one object by ID

--- src/services/CarServices.js
+++ src/services/CarServices.js
@@ -11,5 +11,21 @@ module.exports = {
                 accepted(results);
             });
         })
-    }
+    },
+    getByID: (id) => {
+        return new Promise((accepted, rejected) => {
+            db.query('SELECT * FROM cars WHERE id = ?', [id], (error, results) => {
+                if (error) {
+                    rejected(error);
+                    return;
+                }
+                if (results.length > 0) {
+                    accepted(results[0]);
+                } else {
+                    accepted(false);
+                }
+            });
+
+        })
+    },
 };
